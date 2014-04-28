a860701bd55e29470a208ef212e07e39c871b0fa
Jouni Koivuviita <jouni@jouni.me>
2014-04-29 00:06:39 +0300

    Removed old Dom and Snappy files

--- src/org/vaadin/jouni/animator/client/SnappyServerRpc.java
+++
@@ -1,9 +0,0 @@
-package org.vaadin.jouni.animator.client;
-
-import com.vaadin.shared.communication.ServerRpc;
-
-public interface SnappyServerRpc extends ServerRpc {
-
-    public void actionDone(Action action);
-
-}
